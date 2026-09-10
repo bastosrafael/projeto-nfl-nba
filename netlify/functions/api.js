4dd50d2efa02f83624255ad4394c84a73ee6c882
Rafael Bastos <rafaelwebbr@gmail.com>
2026-09-10 16:57:47 -0300

    fix: preserve original Express path in Netlify api function via serverless-http basePath

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../../backend/app');
 
-exports.handler = serverless(app);
+exports.handler = serverless(app, { basePath: '/.netlify/functions/api' });
